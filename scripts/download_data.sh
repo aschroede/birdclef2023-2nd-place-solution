ad94d561588315049eac64690d922b0cc1277639
Andrew Schroeder <aschroeder@cn84.science.ru.nl>
2025-05-31 11:43:40 +0200

    download_data changed

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -8,31 +8,31 @@ cd "$SCRIPT_DIR" || exit 1
 # set environment variables
 source ../.env 2> /dev/null || source .env
 
-# default directory to save files in
-DIR="$SCRIPT_DIR"/../data/cifar10
-mkdir -p "$DIR"
+# # default directory to save files in
+# DIR="$SCRIPT_DIR"/../data/cifar10
+# mkdir -p "$DIR"
 
 
 
-PROJECT_ROOT="$( dirname "$SCRIPT_DIR" )"
-INPUT_DIR="$PROJECT_ROOT/inputs"
-AUDIO_DIR="$INPUT_DIR/train_audios"
-NOISE_DIR="$INPUT_DIR/background_noise"
+# PROJECT_ROOT="$( dirname "$SCRIPT_DIR" )"
+# INPUT_DIR="$PROJECT_ROOT/inputs"
+# AUDIO_DIR="$INPUT_DIR/train_audios"
+# NOISE_DIR="$INPUT_DIR/background_noise"
 
 
-# --- Download train.csv metadata ---
-echo "📥 Downloading metadata: train.csv"
-cd "$INPUT_DIR"
-kaggle datasets download honglihang/birdclef2023-extended-train
-unzip -o birdclef2023-extended-train.zip
-rm birdclef2023-extended-train.zip
+# # --- Download train.csv metadata ---
+# echo "📥 Downloading metadata: train.csv"
+# cd "$INPUT_DIR"
+# kaggle datasets download honglihang/birdclef2023-extended-train
+# unzip -o birdclef2023-extended-train.zip
+# rm birdclef2023-extended-train.zip
 
-# --- Download BirdCLEF2023 audio files ---
-echo "🎧 Downloading BirdCLEF2023 train audios"
-cd "$AUDIO_DIR"
-kaggle competitions download -c birdclef-2023
-unzip -o birdclef-2023.zip
-rm birdclef-2023.zip
+# # --- Download BirdCLEF2023 audio files ---
+# echo "🎧 Downloading BirdCLEF2023 train audios"
+# cd "$AUDIO_DIR"
+# kaggle competitions download -c birdclef-2023
+# unzip -o birdclef-2023.zip
+# rm birdclef-2023.zip
 
 # --- Download background noise ---
 echo "🔊 Downloading background noise files"
