69e721bc04ddd166afe2962a6fd7ea8a5f3bd63d
aschroede <aschroed@ualberta.ca>
2025-06-01 21:03:46 +0200

    Stuff

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -36,10 +36,10 @@ rm birdclef-2023.zip
 
 # Flatten the directory
 cd "$PROJECT_ROOT"
-!find ./inputs/train_audios -type f -name '*.ogg' -exec mv {} ./inputs/train_audios_flat/ \;
-
-!rm -rf ./inputs/train_audios/
-!mv ./inputs/train_audios_flat ./inputs/train_audios/
+mkdir -p ./inputs/train_audios_flat
+find ./inputs/train_audios -type f -name '*.ogg' -exec mv {} ./inputs/train_audios_flat/ \;
+rm -rf ./inputs/train_audios/
+mv ./inputs/train_audios_flat ./inputs/train_audios/
 
 
 # # --- Download background noise ---
